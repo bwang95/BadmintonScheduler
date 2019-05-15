84e001c3009ca1e1349bb05af388e7a95aeb9d5c
Brian Wang <b.wang@utexas.edu>
2019-05-15 00:28:18 -0700

    Get rid of a redundant null case in the MainActivity

--- app/src/main/java/com/cerridan/badmintonscheduler/MainActivity.kt
+++ app/src/main/java/com/cerridan/badmintonscheduler/MainActivity.kt
@@ -55,7 +55,6 @@ class MainActivity : AppCompatActivity() {
           when (it) {
             COURTS -> CourtsFragment()
             PLAYERS -> PlayersFragment()
-            null -> throw IllegalArgumentException("Missing Fragment type")
           }
         }
         .doOnNext { drawerLayout.closeDrawer(drawer) }
